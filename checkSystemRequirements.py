d30a653cb1b66d254e4e27fd00469de4533540d7
Goo-aw233 <GodDog322@gmail.com>
2024-08-31 01:02:52 +0800

    style: Updating variable names

--- checkSystemRequirements.py
+++ checkSystemRequirements.py
@@ -7,8 +7,8 @@ def check_system_requirements(locale):
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion") as key:
             current_build_number = winreg.QueryValueEx(key, "CurrentBuildNumber")[0]
             if int(current_build_number) < 19042:
-                return translator.translate("version_check")
+                return translator.translate("failure_to_meet_system_requirements")
             else:
-                return translator.translate("system_requirements_met")
+                return translator.translate("meet_system_requirements")
     except FileNotFoundError:
         return translator.translate("cannot_read_version")
