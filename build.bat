1e9dda6a035a338aa1f15c26885e912d7bc4df19
Goo-aw233 <GodDog322@gmail.com>
2024-08-31 20:00:56 +0800

    git commit -m "chore: Update build scripts"

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onefile --windowed --name "MSPCManagerHelper_Preview_v24831_-_we11B" --add-data "locales;locales" MainWindow.py
+pyinstaller --onefile --windowed --name "MSPCManagerHelper_Preview_v24831_-_we11D" --add-data "locales;locales" MainWindow.py
 pause
