3de5edae608771aabaf100424ed74e53e6e0abf3
Goo-aw233 <GodDog322@gmail.com>
2024-09-08 16:51:24 +0800

    feat: Main feature

---
+++ mainFeature.py
@@ -0,0 +1,9 @@
+class MainFeature:
+    def __init__(self, translator):
+        self.translator = translator
+
+    def repair_pc_manager(self):
+        return self.translator.translate("feature_unavailable")
+
+    def get_pc_manager_logs(self):
+        return self.translator.translate("feature_unavailable")
