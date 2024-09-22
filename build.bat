89f7d16b827f26f8a8ae38a22c9d9315b58e3da1
Goo-aw233 <GodDog322@gmail.com>
2024-09-22 17:57:39 +0800

    chore: Update build scripts

--- build.bat
+++ build.bat
@@ -7,9 +7,9 @@ echo %arch%
 echo.
 
 if "%arch%"=="AMD64" (
-    pyinstaller.exe --onefile --windowed --name "MSPCManagerHelper_Preview_v24917_-_we11B_x64" --add-data "locales;locales" --add-binary "tools\procdump\procdump64.exe;." --clean --version-file=version_x64.txt mainWindow.py
+    pyinstaller.exe --onefile --windowed --name "MSPCManagerHelper_Preview_v24922_-_we11A_x64" --add-data "locales;locales" --add-binary "tools\procdump\procdump64.exe;." --clean --version-file=version_x64.txt mainWindow.py
 ) else if "%arch%"=="ARM64" (
-    pyinstaller.exe --onefile --windowed --name "MSPCManagerHelper_Preview_v24917_-_we11B_ARM64" --add-data "locales;locales" --add-binary "tools\procdump\procdump64a.exe;." --clean --version-file=version_ARM64.txt mainWindow.py
+    pyinstaller.exe --onefile --windowed --name "MSPCManagerHelper_Preview_v24922_-_we11A_ARM64" --add-data "locales;locales" --add-binary "tools\procdump\procdump64a.exe;." --clean --version-file=version_ARM64.txt mainWindow.py
 ) else (
     echo UNKNOWN: %arch%
 )
