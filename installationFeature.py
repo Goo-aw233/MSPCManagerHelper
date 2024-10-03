12ea28b8a422972eb7a4ff777733ab60f8a56eb3
Goo-aw233 <GodDog322@gmail.com>
2024-10-03 17:20:48 +0800

    feat: Install Microsoft PC Manager from AppxManifest

--- installationFeature.py
+++ installationFeature.py
@@ -4,8 +4,8 @@ import subprocess
 import requests
 import tempfile
 import webbrowser
-from tkinter import filedialog
-from tkinter import messagebox
+import winreg
+from tkinter import filedialog, messagebox
 
 class InstallationFeature:
     def __init__(self, translator):
@@ -46,48 +46,54 @@ class InstallationFeature:
             return self.translator.translate("install_for_all_users_no_file_selected")
 
         # 弹出提示框询问用户是否选择许可证文件
-        response_for_all_users_license = messagebox.askyesno(
+        response_for_all_users_license = messagebox.askyesnocancel(
             self.translator.translate("install_for_all_users_license_select_notice"),
             self.translator.translate("install_for_all_users_license_select")
         )
 
-        all_users_dependency_package_path = None
+        all_users_dependency_package_paths = None
         if response_for_all_users_license:  # 使用许可证
             all_users_license_path = filedialog.askopenfilename(
                 filetypes=[("License.xml", "*.xml"), ("*", "*")])
             if not all_users_license_path:
                 return self.translator.translate("install_for_all_users_no_file_selected")
+        elif response_for_all_users_license == None:
+            return self.translator.translate("user_canceled")
         else:
             all_users_license_path = None
 
         # 弹出提示框询问用户是否选择依赖包
-        response_for_all_users_dependency = messagebox.askyesno(
+        response_for_all_users_dependency = messagebox.askyesnocancel(
             self.translator.translate("install_for_all_users_dependency_package_select_notice"),
             self.translator.translate("install_for_all_users_dependency_package_select")
         )
 
         if response_for_all_users_dependency:  # 选择依赖包
-            all_users_dependency_package_path = filedialog.askopenfilename(
+            all_users_dependency_package_paths = filedialog.askopenfilenames(
                 filetypes=[("Msix", "*.msix"),
                            ("*", "*")])
-            if not all_users_dependency_package_path:
+            if not all_users_dependency_package_paths:
                 return self.translator.translate("install_for_all_users_no_file_selected")
+        elif response_for_all_users_dependency == None:
+            return self.translator.translate("user_canceled")
 
         try:
             # 构建 Dism.exe 命令
             all_users_dism_command = ['Dism.exe', '/Online', '/Add-ProvisionedAppxPackage',
-                            f'/PackagePath:{all_users_application_package_file_path}']
+                                      f'/PackagePath:{all_users_application_package_file_path}']
 
-            if all_users_license_path: # 使用许可证文件
+            if all_users_license_path:  # 使用许可证文件
                 all_users_dism_command.append(f'/LicensePath:{all_users_license_path}')
-            else: # 不使用许可证文件
+            else:  # 不使用许可证文件
                 all_users_dism_command.append('/SkipLicense')
 
-            if all_users_dependency_package_path: # 使用依赖包，若不使用则跳过
-                all_users_dism_command.insert(-1, f'/DependencyPackagePath:{all_users_dependency_package_path}')
+            if all_users_dependency_package_paths:  # 使用依赖包，若不使用则跳过
+                for dependency_path in all_users_dependency_package_paths:
+                    all_users_dism_command.append(f'/DependencyPackagePath:{dependency_path}')
 
             # 使用 Dism.exe 安装应用
-            result = subprocess.run(all_users_dism_command, capture_output=True, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            result = subprocess.run(all_users_dism_command, capture_output=True, text=True,
+                                    creationflags=subprocess.CREATE_NO_WINDOW)
 
             if result.returncode == 0:
                 return self.translator.translate("install_for_all_users_success")
@@ -106,25 +112,28 @@ class InstallationFeature:
             return self.translator.translate("install_for_current_user_no_file_selected")
 
         # 弹出提示框询问用户是否选择依赖包
-        response_for_current_user_dependency = messagebox.askyesno(
+        response_for_current_user_dependency = messagebox.askyesnocancel(
             self.translator.translate("install_for_current_user_dependency_package_select_notice"),
             self.translator.translate("install_for_current_user_dependency_package_select")
         )
 
-        current_user_dependency_package_path = None
+        current_user_dependency_package_paths = None
         if response_for_current_user_dependency:  # 选择依赖包
-            current_user_dependency_package_path = filedialog.askopenfilename(
+            current_user_dependency_package_paths = filedialog.askopenfilenames(
                 filetypes=[("Msix", "*.msix"),
                            ("*", "*")])
-            if not current_user_dependency_package_path:
+            if not current_user_dependency_package_paths:
                 return self.translator.translate("install_for_current_user_no_file_selected")
+        elif response_for_current_user_dependency == None:
+            return self.translator.translate("user_canceled")
 
         try:
             # 构建 Add-AppxPackage 命令
             command = ['powershell.exe', '-Command',
-                       f'Add-AppxPackage -Path {current_user_application_package_file_path}']
-            if current_user_dependency_package_path: # 使用依赖包，若不使用则跳过
-                command.append(f'-DependencyPackages {current_user_dependency_package_path}')
+                       f'Add-AppxPackage -Path "{current_user_application_package_file_path}"']
+            if current_user_dependency_package_paths:  # 使用依赖包，若不使用则跳过
+                dependency_paths = ",".join([f'"{additional_dependency_paths}"' for additional_dependency_paths in current_user_dependency_package_paths])
+                command.append(f'-DependencyPath {dependency_paths}')
 
             # 执行 Add-AppxPackage 命令安装应用
             result = subprocess.run(command, capture_output=True, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
@@ -147,25 +156,29 @@ class InstallationFeature:
             return self.translator.translate("update_from_application_package_no_file_selected")
 
         # 弹出提示框询问用户是否选择依赖包
-        response_for_update_dependency = messagebox.askyesno(
+        response_for_update_dependency = messagebox.askyesnocancel(
             self.translator.translate("update_from_application_package_dependency_package_select_notice"),
             self.translator.translate("update_from_application_package_dependency_package_select")
         )
 
-        update_dependency_package_path = None
+        update_dependency_package_paths = None
         if response_for_update_dependency:  # 选择依赖包
-            update_dependency_package_path = filedialog.askopenfilename(
+            update_dependency_package_paths = filedialog.askopenfilenames(
                 filetypes=[("Msix", "*.msix"),
                            ("*", "*")])
-            if not update_dependency_package_path:
+            if not update_dependency_package_paths:
                 return self.translator.translate("update_from_application_package_no_file_selected")
+        elif response_for_update_dependency == None:
+            return self.translator.translate("user_canceled")
 
         try:
             # 构建 Add-AppxPackage 命令
             command = ['powershell.exe', '-Command',
-                       f'Add-AppxPackage -Path {update_application_package_file_path}']
-            if update_dependency_package_path:  # 使用依赖包，若不使用则跳过
-                command.append(f'-DependencyPackages {update_dependency_package_path}')
+                       f'Add-AppxPackage -Path "{update_application_package_file_path}"']
+            if update_dependency_package_paths:  # 使用依赖包，若不使用则跳过
+                dependency_paths = ",".join([f'"{additional_dependency_paths}"' for additional_dependency_paths in
+                                             update_dependency_package_paths])
+                command.append(f'-DependencyPath {dependency_paths}')
 
             # 执行 Add-AppxPackage 命令安装应用
             result = subprocess.run(command, capture_output=True, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
@@ -179,7 +192,159 @@ class InstallationFeature:
             return self.translator.translate("update_from_application_package_error") + f": {str(e)}"
 
     def install_from_appxmanifest(self):
-        return self.translator.translate("feature_unavailable")
+        # 弹出警告提示框
+        response = messagebox.askyesnocancel(
+            self.translator.translate("install_from_appxmanifest_warn_title"),
+            self.translator.translate("install_from_appxmanifest_warn")
+        )
+
+        if response is None or not response:
+            return self.translator.translate("user_canceled")
+
+        # 打开文件选择对话框选择文件
+        appxmanifest_file_path = filedialog.askopenfilename(
+            filetypes=[("Msix/MsixBundle", "*.msix;*.msixbundle"), ("*", "*")]
+        )
+
+        if not appxmanifest_file_path:
+            return self.translator.translate("user_canceled")
+
+        try:
+            # 创建临时目录
+            temp_appxmanifest_file_path = os.path.join(tempfile.gettempdir(), "MSPCManagerHelper")
+            if not os.path.exists(temp_appxmanifest_file_path):
+                os.makedirs(temp_appxmanifest_file_path)
+
+            # 复制文件到临时目录并重命名为 .zip
+            file_name = os.path.basename(appxmanifest_file_path)
+            zip_file_path = os.path.join(temp_appxmanifest_file_path, file_name + ".zip")
+            shutil.copyfile(appxmanifest_file_path, zip_file_path)
+
+            # 解压文件
+            pc_manager_unpacked_path = os.path.join(temp_appxmanifest_file_path, os.path.splitext(file_name)[0])
+            shutil.unpack_archive(zip_file_path, pc_manager_unpacked_path)
+
+            # 检测解压后的文件夹内是否还有 .msix 文件
+            msix_files = [f for f in os.listdir(pc_manager_unpacked_path) if f.endswith('.msix')]
+            if not msix_files:
+                # 写入 exclude.txt
+                exclude_file_path = os.path.join(temp_appxmanifest_file_path, "exclude.txt")
+                with open(exclude_file_path, 'w') as exclude_file:
+                    exclude_file.write("AppxMetadata\n")
+                    exclude_file.write("[Content_Types].xml\n")
+                    exclude_file.write("AppxBlockMap.xml\n")
+                    exclude_file.write("AppxSignature.p7x\n")
+            else:
+                # 检查文件名中是否带有 x64 或 arm64
+                x64_file = next((f for f in msix_files if 'x64' in f), None)
+                arm64_file = next((f for f in msix_files if 'arm64' in f), None)
+                if not x64_file and not arm64_file:
+                    return self.translator.translate("install_from_appxmanifest_no_match_pc_manager_architecture")
+
+                # 读取 PROCESSOR_ARCHITECTURE 的值
+                processor_architecture = winreg.QueryValueEx(
+                    winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                   r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment"),
+                    "PROCESSOR_ARCHITECTURE"
+                )[0]
+
+                if processor_architecture == "AMD64" and x64_file:
+                    zip_file_path = os.path.join(temp_appxmanifest_file_path, x64_file + ".zip")
+                    shutil.copyfile(os.path.join(pc_manager_unpacked_path, x64_file), zip_file_path)
+                    shutil.unpack_archive(zip_file_path,
+                                          os.path.join(temp_appxmanifest_file_path, os.path.splitext(x64_file)[0]))
+                    pc_manager_unpacked_path = os.path.join(temp_appxmanifest_file_path, os.path.splitext(x64_file)[0])
+                elif processor_architecture == "ARM64" and arm64_file:
+                    zip_file_path = os.path.join(temp_appxmanifest_file_path, arm64_file + ".zip")
+                    shutil.copyfile(os.path.join(pc_manager_unpacked_path, arm64_file), zip_file_path)
+                    shutil.unpack_archive(zip_file_path,
+                                          os.path.join(temp_appxmanifest_file_path, os.path.splitext(arm64_file)[0]))
+                    pc_manager_unpacked_path = os.path.join(temp_appxmanifest_file_path,
+                                                            os.path.splitext(arm64_file)[0])
+                else:
+                    shutil.rmtree(temp_appxmanifest_file_path)
+                    return self.translator.translate("install_from_appxmanifest_no_match_architecture")
+
+                # 写入 exclude.txt
+                exclude_file_path = os.path.join(temp_appxmanifest_file_path, "exclude.txt")
+                with open(exclude_file_path, 'w') as exclude_file:
+                    exclude_file.write("AppxMetadata\n")
+                    exclude_file.write("[Content_Types].xml\n")
+                    exclude_file.write("AppxBlockMap.xml\n")
+                    exclude_file.write("AppxSignature.p7x\n")
+
+            # 将最后解压的文件夹复制到 %ProgramFiles% 下，除了 exclude.txt 列出的文件与文件夹
+            program_files_path = os.path.join(os.environ['ProgramFiles'], os.path.basename(pc_manager_unpacked_path))
+            if not os.path.exists(program_files_path):
+                os.makedirs(program_files_path)
+
+            with open(exclude_file_path, 'r') as exclude_file:
+                exclude_list = exclude_file.read().splitlines()
+
+            for root, dirs, files in os.walk(pc_manager_unpacked_path):
+                relative_path = os.path.relpath(root, pc_manager_unpacked_path)
+                if relative_path in exclude_list:
+                    continue
+
+                dest_dir = os.path.join(program_files_path, relative_path)
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+
+                for file in files:
+                    if file in exclude_list:
+                        continue
+                    shutil.copyfile(os.path.join(root, file), os.path.join(dest_dir, file))
+
+            # 修改 AppxManifest.xml 文件
+            appxmanifest_path = os.path.join(program_files_path, "AppxManifest.xml")
+            with open(appxmanifest_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+
+            with open(appxmanifest_path, 'w', encoding='utf-8') as file:
+                for line in lines:
+                    if '<TargetDeviceFamily' in line and not line.strip().startswith('<!--'):
+                        indent = line[:len(line) - len(line.lstrip())]
+                        file.write(f"{indent}<!-- {line.strip()} -->\n")
+                        file.write(
+                            f"{indent}<TargetDeviceFamily Name=\"Windows.Universal\" MinVersion=\"10.0.17763.0\" MaxVersionTested=\"10.0.22621.0\"/>\n")
+                    else:
+                        file.write(line)
+
+            # 弹出提示框询问用户是否选择依赖包
+            response_for_dependency = messagebox.askyesnocancel(
+                self.translator.translate("install_from_appxmanifest_dependency_package_select_notice"),
+                self.translator.translate("install_from_appxmanifest_dependency_package_select")
+            )
+
+            if response_for_dependency:  # 选择依赖包
+                dependency_package_paths = filedialog.askopenfilenames(
+                    filetypes=[("Msix", "*.msix"), ("*", "*")])
+                if not dependency_package_paths:
+                    return self.translator.translate("install_from_appxmanifest_no_file_selected")
+
+                for dependency_path in dependency_package_paths:
+                    subprocess.run(
+                        ['powershell.exe', '-Command', f'Add-AppxPackage -Path "{dependency_path}"'],
+                        capture_output=True, text=True, creationflags=subprocess.CREATE_NO_WINDOW
+                    )
+
+            # 注册 AppxManifest.xml
+            subprocess.run(
+                ['powershell.exe', '-Command', f'Add-AppxPackage -Register "{program_files_path}\\AppxManifest.xml"'],
+                capture_output=True, text=True, creationflags=subprocess.CREATE_NO_WINDOW
+            )
+
+            # 删除临时文件
+            for file in os.listdir(temp_appxmanifest_file_path):
+                file_path = os.path.join(temp_appxmanifest_file_path, file)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+
+            return self.translator.translate("install_from_appxmanifest_success")
+        except Exception as e:
+            return f"{self.translator.translate('install_from_appxmanifest_error')}: {str(e)}"
 
     def install_wv2_runtime(self, app):
         wv2_installer_temp_dir = os.path.join(tempfile.gettempdir(), "MSPCManagerHelper")
