b6df8f1b741b4dec1b0fb08763600ba737736ab2
Goo-aw233 <GodDog322@gmail.com>
2024-10-07 14:56:37 +0800

    install_requirements.bat

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,5 +1,7 @@
 @echo off
+cd %~dp0
 pip.exe install -r requirements.txt
 pip.exe install requests
 python.exe -m pip install --upgrade pip
 pause
+exit
--- install_requirements_.venv.bat
+++ install_requirements_.venv.bat
@@ -1,7 +1,17 @@
 @echo off
+setlocal
+
+cd %~dp0
+echo ".venv"
 python -m venv .venv
-.venv\Scripts\activate
+echo "activate"
+call .venv\Scripts\activate
+
+echo "install & upgrade pip"
 pip.exe install -r requirements.txt
 pip.exe install requests
 python.exe -m pip install --upgrade pip
+
 pause
+endlocal
+exit
