44541c5329e5c5974a822cfbaba03f9b11225dca
LuYang114 <GeeBoNeko@gmail.com>
2024-11-03 13:02:18 +0800

    feat: Add the language loading function

--- translator.py
+++ translator.py
@@ -4,13 +4,14 @@ import os
 class Translator:
     def __init__(self, locale):
         self.locale = locale
-        self.translations = self.load_translations()
+        self.load_translations()
 
-    def load_translations(self):
-        base_path = os.path.dirname(__file__)
-        file_path = os.path.join(base_path, 'locales', f'{self.locale}.json')
+    def load_translations(self, file_path=None):
+        if file_path == None:
+            base_path = os.path.dirname(__file__)
+            file_path = os.path.join(base_path, 'locales', f'{self.locale}.json')
         with open(file_path, 'r', encoding='utf-8') as file:
-            return json.load(file)
+            self.translations = json.load(file)
 
     def translate(self, key):
         return self.translations.get(key, key)
