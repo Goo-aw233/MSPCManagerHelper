123d45cd58b42a811be5c33f652c3178f10a612c
Goo-aw233 <GodDog322@gmail.com>
2024-12-15 16:27:17 +0800

    fix: Fixed the issue that switching to another language may not work after loading the language

--- translator.py
+++ translator.py
@@ -2,9 +2,9 @@ import json
 import os
 
 class Translator:
-    def __init__(self, locale):
+    def __init__(self, locale=None, file_path=None):
         self.locale = locale
-        self.load_translations()
+        self.load_translations(file_path)
 
     def load_translations(self, file_path=None):
         if file_path == None:
@@ -12,6 +12,8 @@ class Translator:
             file_path = os.path.join(base_path, 'locales', f'{self.locale}.json')
         with open(file_path, 'r', encoding='utf-8') as file:
             self.translations = json.load(file)
+        self.locale = file_path
+        self.current_language = self.translate(self.translate("current_language"))
 
     def translate(self, key):
         return self.translations.get(key, key)
