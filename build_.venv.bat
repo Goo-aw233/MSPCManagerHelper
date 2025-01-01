7bef8e0e65dbc4fe5248b9a3006ba4b7eb3af6b3
Goo-aw233 <GodDog322@gmail.com>
2025-01-01 16:48:24 +0800

    chore: Update build scripts

--- build_.venv.bat
+++ build_.venv.bat
@@ -11,7 +11,7 @@ if "%arch%"=="AMD64" (
     "%~dp0\.venv\Scripts\pyinstaller.exe" ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.6_x64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.7_x64" ^
         --add-data "locales;locales" ^
         --add-data "assets\\MSPCManagerHelper-256.ico;assets" ^
         --add-binary "tools\\procdump\\procdump64.exe;tools/procdump" ^
@@ -24,7 +24,7 @@ if "%arch%"=="AMD64" (
     "%~dp0\.venv\Scripts\pyinstaller.exe" ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.6_ARM64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.7_ARM64" ^
         --add-data "locales;locales" ^
         --add-data "assets\\MSPCManagerHelper-256.ico;assets" ^
         --add-binary "tools\\procdump\\procdump64a.exe;tools/procdump" ^
