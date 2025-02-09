fa3bef86c254d0b61d6d88448fd0abb9d3dca922
Goo-aw233 <GodDog322@gmail.com>
2025-02-09 16:10:36 +0800

    feat: Prompts the user to turn off `Administrator protect` when it detects that it is on

--- src/checkSystemRequirements.py
+++ src/checkSystemRequirements.py
@@ -1,13 +1,34 @@
 import winreg
-from translator import Translator
 
-def check_system_requirements(translator):
-    try:
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion") as key:
-            current_build_number = winreg.QueryValueEx(key, "CurrentBuildNumber")[0]
-            if int(current_build_number) < 19042:
-                return translator.translate("failure_to_meet_system_requirements")
-            else:
-                return translator.translate("meet_system_requirements")
-    except FileNotFoundError:
-        return translator.translate("cannot_read_version")
+class CheckSystemRequirements:
+    def __init__(self, translator):
+        self.translator = translator
+
+    def check_system_requirements(self):
+        try:
+            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion") as key:
+                current_build_number = winreg.QueryValueEx(key, "CurrentBuildNumber")[0]
+                if int(current_build_number) < 19042:
+                    return self.translator.translate("failure_to_meet_system_requirements")
+                else:
+                    return self.translator.translate("meet_system_requirements")
+        except FileNotFoundError:
+            return self.translator.translate("cannot_read_version")
+
+    @staticmethod
+    def check_system_build_number_and_admin_approval_mode():
+        try:
+            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion") as key:
+                current_build_number = int(winreg.QueryValueEx(key, "CurrentBuildNumber")[0])
+                if current_build_number < 27774:
+                    return False
+
+            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System") as key:
+                admin_approval_mode = int(winreg.QueryValueEx(key, "TypeOfAdminApprovalMode")[0])
+                if admin_approval_mode == 2:
+                    return True
+        except FileNotFoundError:
+            return False
+
+        return False
