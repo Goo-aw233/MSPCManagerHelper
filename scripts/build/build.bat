a588b65f0702c458d004a6d8b2f2c93224949f55
Goo-aw233 <GodDog322@gmail.com>
2025-02-23 15:30:39 +0800

    chore: Update build scripts

--- scripts/build/build.bat
+++ scripts/build/build.bat
@@ -10,7 +10,7 @@ if "%arch%"=="AMD64" (
     pyinstaller.exe ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.12_x64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.13_x64" ^
         --add-data "%~dp0..\\..\\src\\locales;locales" ^
         --add-data "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-256.ico;assets" ^
         --add-binary "%~dp0..\\..\\src\\tools\\ProcDump\\procdump64.exe;tools\\ProcDump" ^
@@ -19,13 +19,13 @@ if "%arch%"=="AMD64" (
         --distpath "%~dp0..\\..\\dist" ^
         --workpath "%~dp0..\\..\\build" ^
         --version-file="%~dp0version_x64.txt" ^
-        -i "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-48.ico" ^
+        --icon "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-48.ico" ^
         "%~dp0..\\..\\src\\mainWindow.py"
 ) else if "%arch%"=="ARM64" (
     pyinstaller.exe ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.12_ARM64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.13_ARM64" ^
         --add-data "%~dp0..\\..\\src\\locales;locales" ^
         --add-data "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-256.ico;assets" ^
         --add-binary "%~dp0..\\..\\src\\tools\\ProcDump\\procdump64a.exe;tools\\ProcDump" ^
@@ -34,7 +34,7 @@ if "%arch%"=="AMD64" (
         --distpath "%~dp0..\\..\\dist" ^
         --workpath "%~dp0..\\..\\build" ^
         --version-file="%~dp0version_ARM64.txt" ^
-        -i "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-48.ico" ^
+        --icon "%~dp0..\\..\\src\\assets\\MSPCManagerHelper-48.ico" ^
         "%~dp0..\\..\\src\\mainWindow.py"
 ) else (
     echo UNKNOWN: %arch%
