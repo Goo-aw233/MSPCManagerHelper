0b1e46d39e76b18b3ee011620adaa0c64224a109
Goo-aw233 <GodDog322@gmail.com>
2025-10-26 22:41:17 +0800

    chore: Update build scripts

--- scripts/build/build_.venv.bat
+++ scripts/build/build_.venv.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 for /f "tokens=2,*" %%a in ('reg query "HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0" /v ProcessorNameString') do set "cpuName=%%b"
 for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PROCESSOR_ARCHITECTURE') do set "arch=%%a"
@@ -8,10 +8,43 @@ for /f "tokens=2,*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Wi
 for /f "tokens=2,*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v EditionID') do set "editionID=%%b"
 
 echo Environment (.venv)
+ver
 echo %arch%
 echo %cpuName%
 echo %buildLabEx%
-echo %lcuVer%
+
+if defined lcuVer (
+    echo !lcuVer!
+) else (
+    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMajorVersionNumber 2^>nul') do set "maj=%%a"
+    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMinorVersionNumber 2^>nul') do set "min=%%a"
+    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuildNumber 2^>nul') do set "buildNum=%%a"
+    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v UBR 2^>nul') do set "ubr=%%a"
+
+    rem Convert Hexadecimal or Decimal Strings to Decimal (If Conversion Fails, Retain Original String)
+    if defined maj (
+        set /a maj=!maj! 2>nul
+    )
+    if defined min (
+        set /a min=!min! 2>nul
+    )
+    if defined buildNum (
+        set /a buildNum=!buildNum! 2>nul
+    )
+    if defined ubr (
+        set /a ubr=!ubr! 2>nul
+    ) else (
+        set "ubr=0"
+    )
+
+    if defined maj if defined min if defined buildNum (
+        set "lcuVer=!maj!.!min!.!buildNum!.!ubr!"
+        echo !lcuVer!
+    ) else (
+        echo (UNKNOWN LCUVer)
+    )
+)
+
 echo %editionID%
 echo.
 echo ####################
@@ -21,7 +54,7 @@ if "%arch%"=="AMD64" (
     "%~dp0..\..\.venv\Scripts\pyinstaller.exe" ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.16_x64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.17_x64" ^
         --add-data "%~dp0..\\..\\src\\locales;locales" ^
         --add-data "%~dp0..\\..\\src\\assets\\MSPCManagerHelper.ico;assets" ^
         --add-binary "%~dp0..\\..\\src\\tools\\ProcDump\\procdump64.exe;tools\\ProcDump" ^
@@ -31,12 +64,12 @@ if "%arch%"=="AMD64" (
         --workpath "%~dp0..\\..\\build" ^
         --version-file="%~dp0version_x64.txt" ^
         --icon "%~dp0..\\..\\src\\assets\\MSPCManagerHelper.ico" ^
-        "%~dp0..\\..\\src\\mainWindow.py"
+        "%~dp0..\\..\\src\\main.py"
 ) else if "%arch%"=="ARM64" (
     "%~dp0..\..\.venv\Scripts\pyinstaller.exe" ^
         --onefile ^
         --windowed ^
-        --name "MSPCManagerHelper_Beta_v0.2.0.16_ARM64" ^
+        --name "MSPCManagerHelper_Beta_v0.2.0.17_ARM64" ^
         --add-data "%~dp0..\\..\\src\\locales;locales" ^
         --add-data "%~dp0..\\..\\src\\assets\\MSPCManagerHelper.ico;assets" ^
         --add-binary "%~dp0..\\..\\src\\tools\\ProcDump\\procdump64a.exe;tools\\ProcDump" ^
@@ -46,7 +79,7 @@ if "%arch%"=="AMD64" (
         --workpath "%~dp0..\\..\\build" ^
         --version-file="%~dp0version_ARM64.txt" ^
         --icon "%~dp0..\\..\\src\\assets\\MSPCManagerHelper.ico" ^
-        "%~dp0..\\..\\src\\mainWindow.py"
+        "%~dp0..\\..\\src\\main.py"
 ) else (
     echo UNKNOWN: %arch%
 )
