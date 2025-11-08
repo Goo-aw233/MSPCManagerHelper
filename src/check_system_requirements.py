60481b6aa52435f97ed11d8e779fb47f587527cc
Goo-aw233 <GodDog322@gmail.com>
2025-11-08 18:06:55 +0800

    refactor:  Cancelled reading of LCUVer

--- src/check_system_requirements.py
+++ src/check_system_requirements.py
@@ -57,21 +57,24 @@ class CheckSystemRequirements:
     def get_windows_installation_information():
         try:
             with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion") as key:
+                display_version = winreg.QueryValueEx(key, "DisplayVersion")[0]
                 edition_id = winreg.QueryValueEx(key, "EditionID")[0]
                 build_lab_ex = winreg.QueryValueEx(key, "BuildLabEx")[0]
 
+                """
                 try:
                     lcu_ver = winreg.QueryValueEx(key, "LCUVer")[0]
                     if lcu_ver:
-                        return f"Microsoft Windows {lcu_ver} {edition_id}\n{build_lab_ex}"
+                        return f"Microsoft Windows {display_version} {lcu_ver} {edition_id}\n{build_lab_ex}"
                 except OSError:
                     # LCUVer 不存在，读取注册表中的其他版本信息
                     pass
+                """
 
                 major_version = winreg.QueryValueEx(key, "CurrentMajorVersionNumber")[0]
                 minor_version = winreg.QueryValueEx(key, "CurrentMinorVersionNumber")[0]
                 build_number = winreg.QueryValueEx(key, "CurrentBuildNumber")[0]
                 ubr = winreg.QueryValueEx(key, "UBR")[0]
-                return f"Microsoft Windows {major_version}.{minor_version}.{build_number}.{ubr} {edition_id}\n{build_lab_ex}"
+                return f"Microsoft Windows {display_version} {major_version}.{minor_version}.{build_number}.{ubr} {edition_id}\n{build_lab_ex}"
         except (FileNotFoundError, OSError):
             return None
