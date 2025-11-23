046443286a1d96f8490232240de8c7e9c7ca7a9f
Goo-aw233 <GodDog322@gmail.com>
2025-11-23 14:54:42 +0800

    chore: Remove the method of reading LCUVer in the build script

--- scripts/build/build_.venv.cmd
+++ scripts/build/build_.venv.cmd
@@ -13,36 +13,32 @@ echo %arch%
 echo %cpuName%
 echo %buildLabEx%
 
-if defined lcuVer (
+for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMajorVersionNumber 2^>nul') do set "maj=%%a"
+for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMinorVersionNumber 2^>nul') do set "min=%%a"
+for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuildNumber 2^>nul') do set "buildNum=%%a"
+for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v UBR 2^>nul') do set "ubr=%%a"
+
+REM Convert Hexadecimal or Decimal Strings to Decimal (If Conversion Fails, Retain Original String)
+if defined maj (
+    set /a maj=!maj! 2>nul
+)
+if defined min (
+    set /a min=!min! 2>nul
+)
+if defined buildNum (
+    set /a buildNum=!buildNum! 2>nul
+)
+if defined ubr (
+    set /a ubr=!ubr! 2>nul
+) else (
+    set "ubr=0"
+)
+
+if defined maj if defined min if defined buildNum (
+    set "lcuVer=!maj!.!min!.!buildNum!.!ubr!"
     echo !lcuVer!
 ) else (
-    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMajorVersionNumber 2^>nul') do set "maj=%%a"
-    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentMinorVersionNumber 2^>nul') do set "min=%%a"
-    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuildNumber 2^>nul') do set "buildNum=%%a"
-    for /f "tokens=3" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v UBR 2^>nul') do set "ubr=%%a"
-
-    rem Convert Hexadecimal or Decimal Strings to Decimal (If Conversion Fails, Retain Original String)
-    if defined maj (
-        set /a maj=!maj! 2>nul
-    )
-    if defined min (
-        set /a min=!min! 2>nul
-    )
-    if defined buildNum (
-        set /a buildNum=!buildNum! 2>nul
-    )
-    if defined ubr (
-        set /a ubr=!ubr! 2>nul
-    ) else (
-        set "ubr=0"
-    )
-
-    if defined maj if defined min if defined buildNum (
-        set "lcuVer=!maj!.!min!.!buildNum!.!ubr!"
-        echo !lcuVer!
-    ) else (
-        echo (UNKNOWN LCUVer)
-    )
+    echo (UNKNOWN LCUVer)
 )
 
 echo %editionID%
