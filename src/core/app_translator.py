448bb1878eda29c4a6dca360a8c9c74ebe2f4aa0
Goo-aw233 <GodDog322@gmail.com>
2026-01-03 14:46:21 +0800

    feat(core/app_translator): Add Basic i18n Translation Functionality
    - Implement basic i18n translation functionality.

--- src/core/app_translator.py
+++ src/core/app_translator.py
@@ -1,2 +1,16 @@
+import json
+from pathlib import Path
+
+
 class AppTranslator:
-    pass
+    def __init__(self, locale):
+        self.locale = locale
+        self.translations = self.load_translations()
+
+    def load_translations(self):
+        file_path = (Path(__file__).resolve().parents[1] / "assets" / "locales" / f"{self.locale}.json")
+        with file_path.open("r", encoding="utf-8") as file:
+            return json.load(file)
+
+    def translate(self, key):
+        return self.translations.get(key, key)
