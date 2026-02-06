d6596fdedc321d14f684becd933ec25411139773
Goo-aw233 <GodDog322@gmail.com>
2026-02-06 10:11:55 +0800

    feat(core/app_resources.py): File Selection Fallback When Using the x64 Version on ARM64
    - When running the x64 version on ARM64, the tools will automatically choose to use the x64 version of the tools, regardless of the Windows version.

--- src/core/app_resources.py
+++ src/core/app_resources.py
@@ -18,10 +18,10 @@ class AppResources:
         app_base_dir = Path(__file__).resolve().parents[1]
         tool_folder = app_base_dir / "assets" / "tools" / tool_folder
 
-        arch_map = {
-            "AMD64": tool_folder / x64_binary,
-            "ARM64": tool_folder / arm64_binary
-        }
+        x64_path = tool_folder / x64_binary
+        arm64_path = tool_folder / arm64_binary
+
+        arch_key = None
 
         # Method 1: platform.machine()
         try:
@@ -30,26 +30,30 @@ class AppResources:
                 arch_key = "AMD64"
             elif any(k in machine for k in ("arm64", "aarch64")):
                 arch_key = "ARM64"
-            else:
-                arch_key = None
         except Exception:
-            arch_key = None
-
-        if arch_key:
-            path = arch_map.get(arch_key)
-            if path and path.exists():
-                return str(path)
+            pass
 
         # Method 2: winreg
-        try:
-            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment") as key:
-                processor_architecture, _ = winreg.QueryValueEx(key, "PROCESSOR_ARCHITECTURE")
-        except (FileNotFoundError, OSError):
-            return None
+        if not arch_key:
+            try:
+                with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                    r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment") as key:
+                    processor_architecture, _ = winreg.QueryValueEx(key, "PROCESSOR_ARCHITECTURE")
+                    arch_key = processor_architecture
+            except (FileNotFoundError, OSError):
+                pass
 
-        path = arch_map.get(processor_architecture)
-        return str(path) if path and path.exists() else None
+        # Depend on Detected Architecture
+        if arch_key == "ARM64":
+            if arm64_path.exists():
+                return str(arm64_path)
+            if x64_path.exists():
+                return str(x64_path)
+
+        if x64_path.exists():
+               return str(x64_path)
+
+        return None
 
     @staticmethod
     def nsudo_path():
